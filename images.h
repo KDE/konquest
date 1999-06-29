8db5d73ac463092619303478c8caef5b6fb33bdf
John Corey <john@jcorey.org>
1999-06-29 19:39:29 +0000

    Change kde_datadir() references to locate().
    svn path=/trunk/kdegames/konquest/; revision=24640

--- images.h
+++ images.h
@@ -1,16 +1,17 @@
 #ifndef _IMAGES_H
+#include <kstddirs.h>
 #define _IMAGES_H
 
-#define IMAGE_SPLASH 		"konquest-splash.gif"
-#define IMAGE_PLANET_1		"planet1.xpm"
-#define IMAGE_PLANET_2		"planet2.xpm"
-#define IMAGE_PLANET_3		"planet3.xpm"
-#define IMAGE_PLANET_4		"planet4.xpm"
-#define IMAGE_PLANET_5		"planet5.xpm"
-#define IMAGE_PLANET_6		"planet6.xpm"
-#define IMAGE_PLANET_7		"planet7.xpm"
-#define IMAGE_PLANET_8		"planet8.xpm"
-#define IMAGE_PLANET_9		"planet9.xpm"
+#define IMAGE_SPLASH 		locate("appdata", "pics/konquest-splash.gif")
+#define IMAGE_PLANET_1		locate("appdata", "pics/planet1.xpm")
+#define IMAGE_PLANET_2		locate("appdata", "pics/planet2.xpm")
+#define IMAGE_PLANET_3		locate("appdata", "pics/planet3.xpm")
+#define IMAGE_PLANET_4		locate("appdata", "pics/planet4.xpm")
+#define IMAGE_PLANET_5		locate("appdata", "pics/planet5.xpm")
+#define IMAGE_PLANET_6		locate("appdata", "pics/planet6.xpm")
+#define IMAGE_PLANET_7		locate("appdata", "pics/planet7.xpm")
+#define IMAGE_PLANET_8		locate("appdata", "pics/planet8.xpm")
+#define IMAGE_PLANET_9		locate("appdata", "pics/planet9.xpm")
 
 #endif // _IMAGES_H
 
