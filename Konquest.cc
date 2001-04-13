46891db9681d03c7a7f03a363f4862071071a451
Andreas Beckermann <b_mann@gmx.de>
2001-04-13 21:20:37 +0000

    GUI: fixing the about text. was my mistake :-(
    svn path=/trunk/kdegames/konquest/; revision=91923

--- Konquest.cc
+++ Konquest.cc
@@ -17,14 +17,15 @@
 #include "mainwin.h"
 #include "map_widget.h"
 
-static const char *description = I18N_NOOP("The Gnu-Lactic Conquest Project\n\nKDE version by Russ Steffen <rsteffen@bayarea.net>\nSee http://www.ia.net/~rsteffen/konquest.html for more info");
+static const char *description= I18N_NOOP("Galactical KDE Game");
+static const char *text = I18N_NOOP("The Gnu-Lactic Conquest Project\n\nKDE version by Russ Steffen <rsteffen@bayarea.net>\nSee http://www.ia.net/~rsteffen/konquest.html for more info");
 
 int 
 main(int argc, char **argv)
 {
     KAboutData aboutData( "konquest", I18N_NOOP("Konquest"), 
         KONQUEST_VERSION, description, KAboutData::License_GPL, 
-        "Copyright (c) 1999-2001, Developers");
+        "Copyright (c) 1999-2001, Developers", text);
     aboutData.addAuthor("Russ Steffen",0, "rsteffen@ia.net");
     KCmdLineArgs::init( argc, argv, &aboutData );
 
