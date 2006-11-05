827c195e1d35bb5c16655cedd072bee109281a40
Inge Wallin <inge@lysator.liu.se>
2006-11-05 17:49:47 +0000

    Cleanup images.h
    svn path=/trunk/KDE/kdegames/konquest/; revision=602326

--- images.h
+++ images.h
@@ -1,17 +1,18 @@
 #ifndef _IMAGES_H
-#include <kstandarddirs.h>
 #define _IMAGES_H
 
-#define IMAGE_SPLASH 		KStandardDirs::locate("appdata", "pics/konquest-splash.png")
-#define IMAGE_PLANET_1		KStandardDirs::locate("appdata", "pics/planet1.xpm")
-#define IMAGE_PLANET_2		KStandardDirs::locate("appdata", "pics/planet2.xpm")
-#define IMAGE_PLANET_3		KStandardDirs::locate("appdata", "pics/planet3.xpm")
-#define IMAGE_PLANET_4		KStandardDirs::locate("appdata", "pics/planet4.xpm")
-#define IMAGE_PLANET_5		KStandardDirs::locate("appdata", "pics/planet5.xpm")
-#define IMAGE_PLANET_6		KStandardDirs::locate("appdata", "pics/planet6.xpm")
-#define IMAGE_PLANET_7		KStandardDirs::locate("appdata", "pics/planet7.xpm")
-#define IMAGE_PLANET_8		KStandardDirs::locate("appdata", "pics/planet8.xpm")
-#define IMAGE_PLANET_9		KStandardDirs::locate("appdata", "pics/planet9.xpm")
+#include <kstandarddirs.h>
+
+#define IMAGE_SPLASH    KStandardDirs::locate("appdata", "pics/konquest-splash.png")
+#define IMAGE_PLANET_1  KStandardDirs::locate("appdata", "pics/planet1.xpm")
+#define IMAGE_PLANET_2  KStandardDirs::locate("appdata", "pics/planet2.xpm")
+#define IMAGE_PLANET_3  KStandardDirs::locate("appdata", "pics/planet3.xpm")
+#define IMAGE_PLANET_4  KStandardDirs::locate("appdata", "pics/planet4.xpm")
+#define IMAGE_PLANET_5  KStandardDirs::locate("appdata", "pics/planet5.xpm")
+#define IMAGE_PLANET_6  KStandardDirs::locate("appdata", "pics/planet6.xpm")
+#define IMAGE_PLANET_7  KStandardDirs::locate("appdata", "pics/planet7.xpm")
+#define IMAGE_PLANET_8  KStandardDirs::locate("appdata", "pics/planet8.xpm")
+#define IMAGE_PLANET_9  KStandardDirs::locate("appdata", "pics/planet9.xpm")
 
 #endif // _IMAGES_H
 
