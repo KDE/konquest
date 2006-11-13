a6be2550683800ab739fafb250a65b6cbb39dcaa
Inge Wallin <inge@lysator.liu.se>
2006-11-13 20:25:35 +0000

    Cleanup of mapscene
    svn path=/trunk/KDE/kdegames/konquest/; revision=604694

--- mapscene.cc
+++ mapscene.cc
@@ -1,13 +1,15 @@
-#include "images.h"
-#include "mapscene.moc"
-#include "mapitems.h"
-#include "map.h"
-#include "sector.h"
 #include <QGraphicsItem>
 #include <QtDebug>
 
+#include "map.h"
+#include "sector.h"
+#include "images.h"
+#include "mapscene.moc"
+#include "mapitems.h"
+
+
 MapScene::MapScene (Map *map)
-    : QGraphicsScene(),
+  : QGraphicsScene(),
     m_map(map),
     m_selectedPlanetItem(NULL),
     m_planetInfoItem(NULL)
@@ -16,22 +18,26 @@ MapScene::MapScene (Map *map)
     connect( m_map, SIGNAL( mapPopulated() ), this, SLOT( mapUpdate() ) );
 }
 
-void MapScene::mapUpdate() {
-    QGraphicsItem *item;
-    Sector *sector;
+
+void MapScene::mapUpdate()
+{
+    QGraphicsItem  *item;
+    Sector         *sector;
     
     while (items().count() > 0) {
         item = items()[0];
         removeItem(item);
         delete item;
     }
+
     addItem(new BackgroundItem(this));
     for (int i = 0 ; i < BOARD_ROWS ; i++) {
         for (int j = 0 ; j < BOARD_COLS ; j++) {
             sector = m_map->sector(Coordinate(i, j));
             if (sector->hasPlanet()) {
                 PlanetItem *item = new PlanetItem(this, sector);
-                connect(item, SIGNAL(planetItemSelected (PlanetItem *)), this, SLOT(planetItemSelected (PlanetItem *)));
+                connect(item, SIGNAL(planetItemSelected (PlanetItem *)), 
+			this, SLOT(planetItemSelected (PlanetItem *)));
                 item->setZValue(1.0);
                 addItem(item);
             }
@@ -39,31 +45,40 @@ void MapScene::mapUpdate() {
     }
 }
 
-void MapScene::unselectPlanet() {
+
+void MapScene::unselectPlanet()
+{
     if (m_selectedPlanetItem) {
         m_selectedPlanetItem->unselect();
     }
 }
 
-void MapScene::planetItemSelected (PlanetItem *item) {
+
+void MapScene::planetItemSelected (PlanetItem *item)
+{
     if (m_selectedPlanetItem)
         unselectPlanet();
     m_selectedPlanetItem = item;
+
     emit planetSelected(item->sector()->planet());
 }
 
-void MapScene::displayPlanetInfo (Planet *planet, QPoint pos) {
+void MapScene::displayPlanetInfo (Planet *planet, QPoint pos)
+{
     if (!planet && m_planetInfoItem) {
         m_planetInfoItem->hide();
         return;
     }
+
     if (!m_planetInfoItem) {
         m_planetInfoItem = new PlanetInfoItem();
         addItem(m_planetInfoItem);
     }
+
     m_planetInfoItem->show();
     m_planetInfoItem->setPlanet(planet);
     m_planetInfoItem->setZValue(1.5);
-    m_planetInfoItem->moveBy(pos.x()-m_planetInfoItem->x(), pos.y()-m_planetInfoItem->y());
+    m_planetInfoItem->moveBy( pos.x()-m_planetInfoItem->x(),
+			      pos.y()-m_planetInfoItem->y() );
     update();
 }
--- mapscene.h
+++ mapscene.h
@@ -1,35 +1,42 @@
 #ifndef _MAPSCENE_H
 #define _MAPSCENE_H
 
+
 #include <KSvgRenderer>
+
 #include <QGraphicsScene>
+
 
 class Planet;
 class PlanetItem;
 class PlanetInfoItem;
 class Map;
 
+
 class MapScene: public QGraphicsScene {
     Q_OBJECT
     public:
         MapScene(Map *map);
-        KSvgRenderer *renderer() { return m_renderer; }
-        Map *map() { return m_map; }
-        void unselectPlanet();
-        void displayPlanetInfo (Planet *planet, QPoint pos);
+	~MapScene() {}
+
+        KSvgRenderer  *renderer() const  { return m_renderer; }
+        Map           *map()      const  { return m_map; }
+
+        void           unselectPlanet();
+        void           displayPlanetInfo(Planet *planet, QPoint pos);
         
     signals:
         void  planetSelected( Planet * );
         
-    private:
-        Map *m_map;
-        KSvgRenderer *m_renderer;
-        PlanetItem *m_selectedPlanetItem;
-        PlanetInfoItem *m_planetInfoItem;
-        
     private slots:
-        void mapUpdate();
-        void planetItemSelected (PlanetItem *);
+        void  mapUpdate();
+        void  planetItemSelected (PlanetItem *);
+        
+    private:
+        Map             *m_map;
+        KSvgRenderer    *m_renderer;
+        PlanetItem      *m_selectedPlanetItem;
+        PlanetInfoItem  *m_planetInfoItem;
 };
 
 #endif
