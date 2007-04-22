e29711ff894ac0536e3e3be172f12e6047643140
Inge Wallin <inge@lysator.liu.se>
2007-04-22 17:30:47 +0000

    Fix planet naming issue.  This fixes more tan one playing issue. :-)
    svn path=/trunk/KDE/kdegames/konquest/; revision=656904

--- map.cc
+++ map.cc
@@ -57,11 +57,11 @@ Map::populateMap( QList<Player *> &players, Player *neutral,
                   int numNeutralPlanets, QList<Planet *> &thePlanets )
 {
     // Store the planet name in a simple char... Nothing more needed.
-    char index = 64;
+    char planetName = 'A';
 
     // Create a planet for each player
     foreach(Player *plr, players) {
-        QString newName( index++ );
+        QString newName( planetName++ );
         Sector *sect = findRandomFreeSector();
         Planet *plrPlanet = Planet::createPlayerPlanet( sect, plr, newName );
 
@@ -70,7 +70,7 @@ Map::populateMap( QList<Player *> &players, Player *neutral,
 
     for( int x = 0; x < numNeutralPlanets; x++ )
     {
-        QString newName( index++ );
+        QString newName( planetName++ );
         Sector *sect = findRandomFreeSector();
         Planet *neutralPlanet = Planet::createNeutralPlanet( sect, neutral, newName );
 
