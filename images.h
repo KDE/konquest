1d2840452ad3b5d90d86d983bafa1ddbbc9cf9b7
Pierre Ducroquet <pinaraf@pinaraf.info>
2007-11-09 09:52:08 +0000

    Remove the old xpm files, they're useless since months...
    svn path=/trunk/KDE/kdegames/konquest/; revision=734588

--- images.h
+++ images.h
@@ -26,15 +26,6 @@
 
 #define IMAGE_SPLASH    KStandardDirs::locate("appdata", "pics/konquest-splash.png")
 #define IMAGES_SVG      KStandardDirs::locate("appdata", "pics/default_theme.svgz")
-#define IMAGE_PLANET_1  KStandardDirs::locate("appdata", "pics/planet1.xpm")
-#define IMAGE_PLANET_2  KStandardDirs::locate("appdata", "pics/planet2.xpm")
-#define IMAGE_PLANET_3  KStandardDirs::locate("appdata", "pics/planet3.xpm")
-#define IMAGE_PLANET_4  KStandardDirs::locate("appdata", "pics/planet4.xpm")
-#define IMAGE_PLANET_5  KStandardDirs::locate("appdata", "pics/planet5.xpm")
-#define IMAGE_PLANET_6  KStandardDirs::locate("appdata", "pics/planet6.xpm")
-#define IMAGE_PLANET_7  KStandardDirs::locate("appdata", "pics/planet7.xpm")
-#define IMAGE_PLANET_8  KStandardDirs::locate("appdata", "pics/planet8.xpm")
-#define IMAGE_PLANET_9  KStandardDirs::locate("appdata", "pics/planet9.xpm")
 
 #endif // _IMAGES_H
 
