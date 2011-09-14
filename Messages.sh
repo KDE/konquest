14648814cf2e3513588333d2413e22ff0a13a9e9
Pierre Ducroquet <pinaraf@pinaraf.info>
2011-09-14 21:49:58 +0000

    Use find for the list of files to translate
    svn path=/trunk/KDE/kdegames/konquest/; revision=1253681

--- Messages.sh
+++ Messages.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 $EXTRACTRC *.rc *.ui >> rc.cpp
-$XGETTEXT *.cc *.cpp maps/*.cc dialogs/*.cc rc.cpp -o $podir/konquest.pot
+$XGETTEXT `find . -name \*.cc -o -name \*.cpp ` -o $podir/konquest.pot
