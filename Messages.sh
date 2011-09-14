e6a4d099fdde2c307eafd7f563e955bf864eb24d
Albert Astals Cid <tsdgeos@terra.es>
2011-09-14 10:51:26 +0000

    Add maps folder since it has i18n calls in it I suggest you to switch to a find based solution to make sure you do not forget files or include files that do not exist CCMAIL: pinaraf@pinaraf.info
    svn path=/trunk/KDE/kdegames/konquest/; revision=1253445

--- Messages.sh
+++ Messages.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 $EXTRACTRC *.rc *.ui >> rc.cpp
-$XGETTEXT *.cc *.cpp dialogs/*.cc rc.cpp -o $podir/konquest.pot
+$XGETTEXT *.cc *.cpp maps/*.cc dialogs/*.cc rc.cpp -o $podir/konquest.pot
