KDE/konquest

commit 2349a7c1a36ba48fd576ce03dde7547384910d8b

Becai now plays quite well against Default Normal and Default Hard in nearly all situations - 1on1 and FFA. It is mainly tested in games with two other players, 22x22 board with 40 neutral planets with 0-2 neutral production. It works nicely in cumulative games as well. The only known "issue" is that it uses neutral kill percentage for target selection even in blind games, where the player cannot see the kill percentage. REVIEW:112747

players/ai/default/hard_gui.cpp
@@ -21,7 +21,7 @@
 
 
 AiDefaultHardGui::AiDefaultHardGui() :
-    PlayerGui(i18n("Default Hard/Defensive"))
+    PlayerGui(i18n("Default (Hard)"))
 {
 }
 

CMakeLists.txt
@@ -37,6 +37,8 @@ set(konquest_SRCS
     dialogs/scoredlg.cc
     dialogs/fleetdlg.cc
 
+    players/ai/becai/becai.cpp
+    players/ai/becai/becai_gui.cpp
     players/ai/default/default.cpp
     players/ai/default/weak.cpp
     players/ai/default/weak_gui.cpp

dialogs/newgamedlg.cc
@@ -22,6 +22,8 @@
  */
 #include "newgamedlg.h"
 
+#include "../players/ai/becai/becai_gui.h"
+#include "../players/ai/becai/becai.h"
 #include "../players/ai/default/weak_gui.h"
 #include "../players/ai/default/normal_gui.h"
 #include "../players/ai/default/hard_gui.h"
@@ -357,6 +359,7 @@ NewGameDlg::NewGameDlg( QWidget *parent, Game *game)
     m_selectablePlayer.push_back(new AiDefaultNormalGui());
     m_selectablePlayer.push_back(new AiDefaultHardGui());
     // m_selectablePlayer.push_back(new AiExampleGui());
+    m_selectablePlayer.push_back(new AiBecaiGui());
 
     m_w = new NewGameDlgUI(this);
     m_w->map->setMap(m_game->map());

doc/index.docbook
@@ -113,17 +113,19 @@ Listed below are the following parameters for the new game:
 <itemizedlist>
 
 <listitem><para>Players</para><itemizedlist>
-  
+
   <listitem><para>Amount of players with <guibutton>Add</guibutton> or <guibutton>Remove</guibutton> players</para></listitem>
   <listitem><para>Names of players</para></listitem>
   <listitem><para>Type of players (Computer versus Human)</para></listitem>
   <listitem><para>Difficulty of computer players (Low/Normal/Hard)</para></listitem>
   </itemizedlist>
-  <para>To change the name of the player, double-click in the <guilabel>Name</guilabel> 
-  column and edit it. To change the type of the player, open the drop-down list by 
+  <para>To change the name of the player, double-click in the <guilabel>Name</guilabel>
+  column and edit it. To change the type of the player, open the drop-down list by
   double-clicking in the <guilabel>Type</guilabel> column.</para>
-  <para>The type of AI player can be chosen from <guimenuitem>Default Weak</guimenuitem> (a player that is neither aggressive in attacks nor in defense), <guimenuitem>Default Normal/Offensive</guimenuitem> (an aggressive attacking player) and <guimenuitem>Default Hard/Defensive</guimenuitem> (a player with defensive strategy).</para>
-</listitem>  
+  <para>The type of AI player can be chosen from <guimenuitem>Default (Weak)</guimenuitem> (a player that is neither aggressive in attacks nor in defense), <guimenuitem>Default (Offensive)</guimenuitem> (an aggressive attacking player) and <guimenuitem>Default (Defensive)</guimenuitem> (a player with defensive strategy).</para>
+  <para>Besides these classic AI players, <guimenuitem>Becai (Balanced)</guimenuitem> is a balanced player taking most of the <link linkend="strattips" endterm="strattips.title"/> into account.</para>
+  <para>Keep in mind that the actual difficulty of any opponent depends on your very own strategy. If you play aggressively yourself, an aggressive AI player might be easier to defeat than a defensive player, and vice versa.</para>
+</listitem>
 
 <listitem><para>Map</para><itemizedlist>
   
@@ -222,9 +224,10 @@ ships.</para></listitem>
 </sect1>
 
 <sect1 id="strattips">
-<title>Strategies and Tips</title>
+<title id="strattips.title">Strategies and Tips</title>
 <itemizedlist>
-<listitem><para>Do not attack a planet with less than 10 ships.</para></listitem>
+<listitem><para>The game is all about ship production. Try to get as many planets as possible as fast as your can in order to build more ships than any of your opponents.</para></listitem>
+<listitem><para>Do not attack a planet with less than 10 ships, unless you can conquer it.</para></listitem>
 <listitem><para>Attack neutral planets before other players' planets.</para></listitem>
 <listitem><para>Split your ship storages across several planets.</para></listitem>
 <listitem><para>Plan your attacks in advance.</para></listitem>
@@ -391,19 +394,6 @@ Can I store ships on a planet for later use?
 <answer>
 <para>
 Yes, you should however store them on multiple planets to be able to send them to nearby planets.
-</para>
-</answer>
-</qandaentry>
-
-<qandaentry>
-<question>
-<para>
-I have sent out all the ships on a planet, but when I move my mouse over the planet, it shows the ships still in place, why?
-</para>
-</question>
-<answer>
-<para>
-The number of ships on a planet is updated when a turn is ended.
 </para>
 </answer>
 </qandaentry>

players/ai/becai/becai.h
@@ -16,18 +16,32 @@
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
-#include "weak_gui.h"
-#include "weak.h"
+#ifndef AI_BECAI_H
+#define AI_BECAI_H
+
+#include "../../computerplayer.h"
 
 
-AiDefaultWeakGui::AiDefaultWeakGui() :
-    PlayerGui(i18n("Default (Weak)"))
+/**
+ * This is quite a challenging AI.
+ */
+
+class AiBecai : public ComputerPlayer
 {
-}
+    Q_OBJECT
 
+public:
+    explicit AiBecai(Game *game, const QString &newName, const QColor &color);
 
-Player*
-AiDefaultWeakGui::createInstance(Game *game, const QString &newName, const QColor &color) const
-{
-    return new AiDefaultWeak(game, newName, color);
-}
+    virtual void play();
+
+private:
+    int getMinimumDefenceFleetSize(Planet *planet, int minimumBaseDefenceFleetSize, double averageOwnKillPercentage, double averageOwnProduction);
+
+signals:
+
+public slots:
+
+};
+
+#endif // AI_BECAI_H

players/ai/becai/becai_gui.cpp
@@ -16,18 +16,18 @@
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
-#include "weak_gui.h"
-#include "weak.h"
+#include "becai_gui.h"
+#include "becai.h"
 
 
-AiDefaultWeakGui::AiDefaultWeakGui() :
-    PlayerGui(i18n("Default (Weak)"))
+AiBecaiGui::AiBecaiGui() :
+    PlayerGui(i18nc("A distinct unique AI playing a balanced strategy", "Becai (Balanced)"))
 {
 }
 
 
 Player*
-AiDefaultWeakGui::createInstance(Game *game, const QString &newName, const QColor &color) const
+AiBecaiGui::createInstance(Game *game, const QString &newName, const QColor &color) const
 {
-    return new AiDefaultWeak(game, newName, color);
+    return new AiBecai(game, newName, color);
 }

players/ai/becai/becai_gui.h
@@ -16,18 +16,17 @@
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
-#include "weak_gui.h"
-#include "weak.h"
+#ifndef AI_BECAI_GUI_H
+#define AI_BECAI_GUI_H
+
+#include "../../player_gui.h"
 
 
-AiDefaultWeakGui::AiDefaultWeakGui() :
-    PlayerGui(i18n("Default (Weak)"))
+class AiBecaiGui : public PlayerGui
 {
-}
+public:
+    explicit AiBecaiGui();
+    virtual Player* createInstance(Game *game, const QString &newName, const QColor &color) const;
+};
 
-
-Player*
-AiDefaultWeakGui::createInstance(Game *game, const QString &newName, const QColor &color) const
-{
-    return new AiDefaultWeak(game, newName, color);
-}
+#endif // AI_BECAI_GUI_H

commit 400ae70daa8603ef15475a6fca55211f967b3f43

fix AI player controller name
The name should be "Default (Defensive)" so the relative "Hard" is not used. Furthermore it should match the term used in the handbook.

This inconsistency was introduced in 2349a7c1a36ba48fd576ce03dde7547384910d8b.

REVIEW: 113410

players/ai/default/hard_gui.cpp
@@ -21,7 +21,7 @@
 
 
 AiDefaultHardGui::AiDefaultHardGui() :
-    PlayerGui(i18n("Default (Hard)"))
+    PlayerGui(i18n("Default (Defensive)"))
 {
 }
 